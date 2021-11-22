18937f89623ecd82af06e3ea1dbd795b5f31cc70
Michelle Shin <michelles8297@gmail.com>
2021-11-21 17:10:43 -0800

    added two javascript functions for View Page html

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function cleanUpIndex() {
     for (let i = 0; i < cleanUp.length; i++) { cleanUp[i].remove(); }
 }
 
-
-function createSingleIndex (contact) {
+function createSingleIndex(contact) {
     // CREAT DOM node that represents a single index card for index page. 
     // take in single object = object represents a single contact 
     let main = document.querySelectorAll('.main');
+
     let createAtag = document.createElement('a');
     let createDiv = document.createElement('div');
     let createPtag = document.createElement('p');
@@ -22,8 +22,7 @@ function createSingleIndex (contact) {
     main[0].appendChild(createAtag);
 }
 
-
-function renderIndex (contact) {
+function renderIndex(contact) {
     // creates all of DOM nodes that are unique
     // take single parameter, array containing contacts, each represent single contact
     let main = document.querySelectorAll('.main');
@@ -56,3 +55,70 @@ let contactList = [
     email: "whatsup@doc.example.com",
     },
 ]
+
+function cleanUpView() {
+    // removes all DOM node that are unique to index page 
+    let cleanUp = document.querySelectorAll('.contactinfo');
+    cleanUp.remove;
+    for (let i = 0; i < cleanUp.length; i++) {cleanUp[i].remove();}
+}
+
+function renderView(contact) {
+    let main = document.querySelectorAll('.main');
+    let infoDiv = document.createElement('div');
+    infoDiv.className = 'contactinfo';
+    main[0].appendChild(infoDiv);
+
+    let nameDiv = document.createElement('div');
+    nameDiv.className = 'contactname';
+    let nameText = document.createTextNode(contact.name);
+    nameDiv.appendChild(nameText);
+    infoDiv.appendChild(nameDiv);
+
+    let contactImg = document.createElement('img');
+    contactImg.className = 'profilepic';
+    contactImg.src = './img/profile.jpg';
+    contactImg.alt = 'Profile Picture';
+    nameDiv.appendChild(contactImg);
+
+    let emailDiv = document.createElement('div');
+    emailDiv.className = 'contactemail';
+    let emailNode = document.createTextNode('email:' + contact.email);
+    emailDiv.appendChild(emailNode);
+    infoDiv.appendChild(emailDiv);
+
+    let phoneDiv = document.createElement('div');
+    phoneDiv.className = 'contactphone';
+    let phoneNode = document.createTextNode('cell: +1' + contact.phone);
+    phoneDiv.appendChild(phoneNode);
+    infoDiv.appendChild(phoneDiv);
+
+    let addressDiv = document.createElement('div');
+    addressDiv.className = 'contactaddress';
+    let addressNode = document.createTextNode('address:' + contact.address);
+    addressDiv.appendChild(addressNode);
+    infoDiv.appendChild(addressDiv);
+
+    let buttonDiv = document.createElement('div');
+    buttonDiv.className = 'buttons';
+    infoDiv.appendChild(buttonDiv);
+    let editButton = document.createElement('button')
+    editButton.className = 'button edit';
+    let closeButton = document.createElement('button')
+    closeButton.className = 'button close';
+
+    buttonDiv.appendChild(editButton);
+    buttonDiv.appendChild(closeButton);
+    
+    editButton.value = 'Edit'
+    closeButton.value = 'Close'
+    let editButtonText = document.createTextNode('Edit')
+    let closeButtonText = document.createTextNode('Close')
+
+    editButton.appendChild(editButtonText);
+    closeButton.appendChild(closeButtonText);
+
+    main[0].appendChild(infoDiv); 
+}
+
+
